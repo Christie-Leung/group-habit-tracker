724fd2578b4f1d7a60c92a42274ef61f6eced233
Christie Leung <christie@murphyshome.net>
2024-02-12 22:01:16 -0800

    Account signup setup using ClerkJS

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 
-export default function Home() {
+const SetupPage = () => {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -111,3 +114,5 @@ export default function Home() {
     </main>
   );
 }
+
+export default SetupPage;
