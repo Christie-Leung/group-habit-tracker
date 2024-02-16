856207e4b22c6c9480920a7e9e2604fbfd7b53bf
Christie Leung <christie@murphyshome.net>
2024-02-15 22:02:39 -0800

    Add Avatar views for user column

--- app/(root)/(routes)/components/columns.tsx
+++ app/(root)/(routes)/components/columns.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Group } from "@/models/group"
 import { ColumnDef } from "@tanstack/react-table"
 
@@ -11,7 +12,17 @@ export const columns: ColumnDef<Group>[] = [
     },
     {
         accessorKey: "users",
-        header: "Users"
+        header: "Users",
+        cell: ({ row }) => {
+            const firstName: string = row.getValue("firstName");
+            const lastName: string = row.getValue("lastName");
+            return (
+                <Avatar>
+                    <AvatarImage src={row.getValue("profilePic")} />
+                    <AvatarFallback>{firstName[0]}{lastName[0]}</AvatarFallback>
+                </Avatar>
+            )
+        }
     },
     {
         accessorKey: "habits",
